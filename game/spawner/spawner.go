5aa3998cae5e52f70c81b5cff450b1ebf27a8322
Insality <insality@gmail.com>
2024-07-21 12:51:55 +0300

    Migrate to Defold 191

--- game/spawner/spawner.go
+++ game/spawner/spawner.go
@@ -2,284 +2,89 @@ embedded_components {
   id: "player"
   type: "collectionfactory"
   data: "prototype: \"/game/objects/player/player.collection\"\n"
-  "load_dynamically: false\n"
-  "dynamic_prototype: false\n"
   ""
-  position {
-    x: 0.0
-    y: 0.0
-    z: 0.0
-  }
-  rotation {
-    x: 0.0
-    y: 0.0
-    z: 0.0
-    w: 1.0
-  }
 }
 embedded_components {
   id: "bullet"
   type: "collectionfactory"
   data: "prototype: \"/game/objects/bullet/bullet.collection\"\n"
-  "load_dynamically: false\n"
-  "dynamic_prototype: false\n"
   ""
-  position {
-    x: 0.0
-    y: 0.0
-    z: 0.0
-  }
-  rotation {
-    x: 0.0
-    y: 0.0
-    z: 0.0
-    w: 1.0
-  }
 }
 embedded_components {
   id: "explosion"
   type: "collectionfactory"
   data: "prototype: \"/game/objects/explosion/explosion.collection\"\n"
-  "load_dynamically: false\n"
-  "dynamic_prototype: false\n"
   ""
-  position {
-    x: 0.0
-    y: 0.0
-    z: 0.0
-  }
-  rotation {
-    x: 0.0
-    y: 0.0
-    z: 0.0
-    w: 1.0
-  }
 }
 embedded_components {
   id: "enemy"
   type: "collectionfactory"
   data: "prototype: \"/game/objects/enemy/enemy.collection\"\n"
-  "load_dynamically: false\n"
-  "dynamic_prototype: false\n"
   ""
-  position {
-    x: 0.0
-    y: 0.0
-    z: 0.0
-  }
-  rotation {
-    x: 0.0
-    y: 0.0
-    z: 0.0
-    w: 1.0
-  }
 }
 embedded_components {
   id: "background"
   type: "collectionfactory"
   data: "prototype: \"/game/objects/background/background.collection\"\n"
-  "load_dynamically: false\n"
-  "dynamic_prototype: false\n"
   ""
-  position {
-    x: 0.0
-    y: 0.0
-    z: 0.0
-  }
-  rotation {
-    x: 0.0
-    y: 0.0
-    z: 0.0
-    w: 1.0
-  }
 }
 embedded_components {
   id: "wall"
   type: "collectionfactory"
   data: "prototype: \"/game/objects/wall/wall.collection\"\n"
-  "load_dynamically: false\n"
-  "dynamic_prototype: false\n"
   ""
-  position {
-    x: 0.0
-    y: 0.0
-    z: 0.0
-  }
-  rotation {
-    x: 0.0
-    y: 0.0
-    z: 0.0
-    w: 1.0
-  }
 }
 embedded_components {
   id: "enemy_big"
   type: "collectionfactory"
   data: "prototype: \"/game/objects/enemy/enemy_big.collection\"\n"
-  "load_dynamically: false\n"
-  "dynamic_prototype: false\n"
   ""
-  position {
-    x: 0.0
-    y: 0.0
-    z: 0.0
-  }
-  rotation {
-    x: 0.0
-    y: 0.0
-    z: 0.0
-    w: 1.0
-  }
 }
 embedded_components {
   id: "circle"
   type: "collectionfactory"
   data: "prototype: \"/game/objects/circle.collection\"\n"
-  "load_dynamically: false\n"
-  "dynamic_prototype: false\n"
   ""
-  position {
-    x: 0.0
-    y: 0.0
-    z: 0.0
-  }
-  rotation {
-    x: 0.0
-    y: 0.0
-    z: 0.0
-    w: 1.0
-  }
 }
 embedded_components {
   id: "gui_main"
   type: "collectionfactory"
   data: "prototype: \"/gui/gui_main/gui_main.collection\"\n"
-  "load_dynamically: false\n"
-  "dynamic_prototype: false\n"
   ""
-  position {
-    x: 0.0
-    y: 0.0
-    z: 0.0
-  }
-  rotation {
-    x: 0.0
-    y: 0.0
-    z: 0.0
-    w: 1.0
-  }
 }
 embedded_components {
   id: "damage_number"
   type: "collectionfactory"
   data: "prototype: \"/game/objects/damage_number/damage_number.collection\"\n"
-  "load_dynamically: false\n"
-  "dynamic_prototype: false\n"
   ""
-  position {
-    x: 0.0
-    y: 0.0
-    z: 0.0
-  }
-  rotation {
-    x: 0.0
-    y: 0.0
-    z: 0.0
-    w: 1.0
-  }
 }
 embedded_components {
   id: "pit"
   type: "collectionfactory"
   data: "prototype: \"/game/objects/pit/pit.collection\"\n"
-  "load_dynamically: false\n"
-  "dynamic_prototype: false\n"
   ""
-  position {
-    x: 0.0
-    y: 0.0
-    z: 0.0
-  }
-  rotation {
-    x: 0.0
-    y: 0.0
-    z: 0.0
-    w: 1.0
-  }
 }
 embedded_components {
   id: "wall_pit"
   type: "collectionfactory"
   data: "prototype: \"/game/objects/wall/wall_pit.collection\"\n"
-  "load_dynamically: false\n"
-  "dynamic_prototype: false\n"
   ""
-  position {
-    x: 0.0
-    y: 0.0
-    z: 0.0
-  }
-  rotation {
-    x: 0.0
-    y: 0.0
-    z: 0.0
-    w: 1.0
-  }
 }
 embedded_components {
   id: "enemy_rectangle"
   type: "collectionfactory"
   data: "prototype: \"/game/objects/enemy/enemy_rectangle.collection\"\n"
-  "load_dynamically: false\n"
-  "dynamic_prototype: false\n"
   ""
-  position {
-    x: 0.0
-    y: 0.0
-    z: 0.0
-  }
-  rotation {
-    x: 0.0
-    y: 0.0
-    z: 0.0
-    w: 1.0
-  }
 }
 embedded_components {
   id: "rocket"
   type: "collectionfactory"
   data: "prototype: \"/game/objects/bullet/rocket.collection\"\n"
-  "load_dynamically: false\n"
-  "dynamic_prototype: false\n"
   ""
-  position {
-    x: 0.0
-    y: 0.0
-    z: 0.0
-  }
-  rotation {
-    x: 0.0
-    y: 0.0
-    z: 0.0
-    w: 1.0
-  }
 }
 embedded_components {
   id: "bullet_shotgun"
   type: "collectionfactory"
   data: "prototype: \"/game/objects/bullet/bullet_shotgun.collection\"\n"
-  "load_dynamically: false\n"
-  "dynamic_prototype: false\n"
   ""
-  position {
-    x: 0.0
-    y: 0.0
-    z: 0.0
-  }
-  rotation {
-    x: 0.0
-    y: 0.0
-    z: 0.0
-    w: 1.0
-  }
 }
